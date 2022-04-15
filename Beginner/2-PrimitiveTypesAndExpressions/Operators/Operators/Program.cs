f6d7d80ffdf6798539de637a07dddce60621df9b
anthgrim <anthgrim@icloud.com>
2022-04-15 14:02:58 -0400

    Adding Operators

---
+++ Beginner/2-PrimitiveTypesAndExpressions/Operators/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine((float)a / (float)b); //Using casting
+            Console.WriteLine(a + b * c);
+
+            //Comparison Operators
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b));
+
+            //Logical Operators
+            Console.WriteLine(c > b && c > a);
+            Console.WriteLine(c > a && c == b);
+            Console.WriteLine(a < b || b > c);
+        }
+    }
+}
