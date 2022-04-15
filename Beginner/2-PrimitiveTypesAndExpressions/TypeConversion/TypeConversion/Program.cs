5a9ea073a44975ff57964a4d0eba4339f9809cd9
anthgrim <anthgrim@icloud.com>
2022-04-15 14:13:07 -0400

    Adding Type Conversion

---
+++ Beginner/2-PrimitiveTypesAndExpressions/TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Type Conversion
+            byte b = 1;
+            int i = b;
+
+            Console.WriteLine(i);
+
+            //Data Lost
+            int c = 1000;
+            byte d = (byte)c; //Using casting
+
+            Console.WriteLine(d);
+
+            //Non-Compatible Types
+
+            try
+            {
+                var number = "1234";
+                var k = byte.Parse(number);
+                Console.WriteLine(k);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The variable number could not be converted into a byte");
+            }
+
+            try
+            {
+                var str = "true";
+                var isTrue = bool.Parse(str);
+
+                Console.WriteLine(isTrue);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The variable str could not be converted into a boolean");
+            }
+        }
+    }
+}
