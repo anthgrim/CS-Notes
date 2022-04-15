4de5c486471c227a84ec70960b46fcfa9b638920
anthgrim <anthgrim@icloud.com>
2022-04-15 15:33:38 -0400

    Adding Enums

---
+++ Beginner/3-Non-PrimitiveTypes/Enums/Enums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    //Enum Declaration
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shipMethod = ShippingMethod.Express;
+            Console.WriteLine((int)shipMethod); //2
+
+            var shipMethodId = 3;
+            Console.WriteLine((ShippingMethod)shipMethodId); //Express
+
+            var shipMethodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), shipMethodName);
+            Console.WriteLine(shippingMethod); //Express
+        }
+    }
+}
