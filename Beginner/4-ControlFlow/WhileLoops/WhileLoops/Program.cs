4f0fad8b48486323147a2c0a2072ec3b3ee9be4a
anthgrim <anthgrim@icloud.com>
2022-04-16 18:50:27 -0400

    Added While Loop

---
+++ Beginner/4-ControlFlow/WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type your name:");
+                var userInput = Console.ReadLine();
+                
+                if (!String.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Echo: {0}", userInput);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
