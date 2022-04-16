a0aecf2d6bcdf38a4298fd72b7bc1a8ce456f724
anthgrim <anthgrim@icloud.com>
2022-04-16 19:02:47 -0400

    Added Arrays

---
+++ Beginner/5-ArraysAndLists/Arrays/Arrays/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3] {1,2,3};
+
+            //Length
+            Console.WriteLine(numbers.Length);
+
+            //IndexOf
+            var index = Array.IndexOf(numbers, 3);
+            Console.WriteLine("Index of 3: {0}\n", index);
+
+            //Clear
+            Array.Clear(numbers, 0, 2); //Sets to 0, false, or null depending on the type
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Copy
+            var newArray = new int[numbers.Length];
+            Array.Copy(numbers, newArray, numbers.Length);
+            Console.WriteLine("\nCopied Array:");
+            foreach (var number in newArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Sort
+            Console.WriteLine("\nSorting:");
+            Array.Sort(numbers);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Reverse
+            Console.WriteLine("\nReverse:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
