2f7b38ad2c13061b805971bbbe43cfdf071ce61c
anthgrim <anthgrim@icloud.com>
2022-04-16 18:36:48 -0400

    Added If Else and Switch case

---
+++ Beginner/4-ControlFlow/IfElse-SwitchCase/IfElse-SwitchCase/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace IfElse_SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("Morning");
+            }
+            else if (hour > 12 && hour < 18)
+            {
+                Console.WriteLine("Afternoon");
+            }
+            else 
+            {
+                Console.WriteLine("Night");    
+            }
+
+            var isGoldCustomer = true;
+            float price;
+
+            if (isGoldCustomer)
+            {
+                price = 19.95f;
+            }
+            else
+            {
+                price = 29.95f;
+            }
+            Console.WriteLine(price);
+
+            float price2 = (isGoldCustomer ? 19.95f : 29.95f);
+            Console.WriteLine(price2);
+
+            var season = SeasonEnum.Autumn;
+
+            switch (season)
+            {
+                case SeasonEnum.Spring:
+                    Console.WriteLine("It's spring");
+                    break;
+                case SeasonEnum.Summer:
+                    Console.WriteLine("It's summer");
+                    break;
+                case SeasonEnum.Autumn:
+                    Console.WriteLine("It's autumn");
+                    break;
+                default:
+                    Console.WriteLine("It's winter");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Beginner/4-ControlFlow/IfElse-SwitchCase/IfElse-SwitchCase/SeasonEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse_SwitchCase
+{
+    public enum SeasonEnum
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
