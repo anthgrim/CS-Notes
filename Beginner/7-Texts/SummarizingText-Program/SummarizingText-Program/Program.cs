88742ff276e0b5956fa22ea2436297697e81255a
anthgrim <anthgrim@icloud.com>
2022-04-16 22:39:17 -0400

    Added SummarizingText-Program

---
+++ Beginner/7-Texts/SummarizingText-Program/SummarizingText-Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really really really really really text";
+            
+            Console.WriteLine(StringUtility.SummarizeText(sentence,25));
+        }
+    }
+}
---
+++ Beginner/7-Texts/SummarizingText-Program/SummarizingText-Program/StringUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarizingText_Program
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+            else
+            {
+                var words = text.Split(' ');
+                var totalChars = 0;
+                var sumWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    sumWords.Add(word);
+
+                    totalChars += word.Length + 1;
+
+                    if (totalChars > maxLength)
+                    {
+                        break;
+                    }
+                }
+
+                return String.Join(" ", sumWords) + "...";
+            }
+        }
+    }
+}
