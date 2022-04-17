8e25f6a6af5bf942173d0c0de3fd31bfae42f591
anthgrim <anthgrim@icloud.com>
2022-04-17 12:27:55 -0400

    Added String Builders

---
+++ Beginner/7-Texts/StringBuilders/StringBuilders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+
+            //Append
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+            Console.WriteLine(builder);
+
+            //Replace
+            builder.Replace('-', '+');
+            Console.WriteLine(builder);
+
+            //Remove
+            builder.Remove(0, 10);
+            Console.WriteLine(builder);
+
+            //Insert
+            builder.Insert(0, new string('-',10));
+            Console.WriteLine(builder);
+
+            //Index
+            Console.WriteLine("First character: {0}", builder[0]);
+
+        }
+    }
+}
