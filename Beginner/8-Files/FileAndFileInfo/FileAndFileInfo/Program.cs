214bbba36de8c147990bedae4c6c942aa661eca7
anthgrim <anthgrim@icloud.com>
2022-04-17 12:56:39 -0400

    Added FileAndFileInfo

---
+++ Beginner/8-Files/FileAndFileInfo/FileAndFileInfo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+namespace FileAndFileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\temp\example.jpg";
+
+            //File Class
+            //Copy
+            File.Copy(path, @"d:\temp\myfile.jpg", true);
+
+            //Delete
+            File.Delete(path);
+
+            //If exists
+            if (File.Exists(path))
+            {
+                //Code
+            }
+
+            //Read
+            var content = File.ReadAllText(path);
+
+            //FileInfo
+            var fileInfo = new FileInfo(path);
+
+            //CopyTo
+            fileInfo.CopyTo("...");
+
+            //Delete
+            fileInfo.Delete();
+
+            //If exists
+            if (fileInfo.Exists)
+            {
+                //Code
+            }
+        }
+    }
+}
