0cb2801ad14bca1cccf5ff3baad4be44c1c0fcb0
anthgrim <anthgrim@icloud.com>
2022-04-17 13:44:01 -0400

    Path Class

---
+++ Beginner/8-Files/Paths/Paths/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Paths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\Users\Anthg\Documents\CSharp\CS-Notes";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: {0}", Path.GetExtension(path));
+            Console.WriteLine("File Name: {0}", Path.GetFileName(path));
+            Console.WriteLine("File Name without extension: {0}", Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: {0}", Path.GetDirectoryName(path));
+        }
+    }
+}
