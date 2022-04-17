65ff9296cd16dd1c5438f1c3335db73a59240217
anthgrim <anthgrim@icloud.com>
2022-04-16 20:48:20 -0400

    Added dates

---
+++ Beginner/6-Dates/DateTime/DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Date
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015,1,1);
+
+            var now = DateTime.Now;
+
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: {0}",now.Hour);
+            Console.WriteLine("Minute: {0}",now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("mm-dd-yyyy"));
+        }
+    }
+}
