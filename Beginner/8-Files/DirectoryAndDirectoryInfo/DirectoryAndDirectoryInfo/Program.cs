a972d61cc925106de56c9e32cbcba4498384ae2f
anthgrim <anthgrim@icloud.com>
2022-04-17 13:35:37 -0400

    Directory and DirectoryInfo

---
+++ Beginner/8-Files/DirectoryAndDirectoryInfo/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryAndDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Directory
+            var path = @"c:\...";
+            Directory.CreateDirectory(path);
+
+            var files = Directory.GetFiles(path, "*.sln", SearchOption.AllDirectories);
+            
+            foreach (var file in files)
+            {
+                //Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(path,"*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Found");
+            }
+
+            //Directory Info
+            var directoryInfo = new DirectoryInfo(path);
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+        }
+    }
+}
